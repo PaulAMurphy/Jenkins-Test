dfbe4f21e368d064abaeb39cc428becbea85b882
root <root@ip-172-31-22-73.us-east-2.compute.internal>
2020-02-04 15:24:28 +0000

    Jenkins Test.

--- test/test_insert.sql
+++ test/test_insert.sql
@@ -1,1 +1,1 @@
-mysql -uroot  -e "use pol_db;   insert into x_test values (99999);commit;select * from x_test;"
+mysql -uroot  -e "use pol_db;   insert into x_test values (676767);commit;select * from x_test;"
