16a217d5dc6212de6447de641a82ade542797b4f
root <root@ip-172-31-22-73.us-east-2.compute.internal>
2020-02-04 15:22:00 +0000

    Jenkins Test.

--- test/test.sh
+++ test/test.sh
@@ -1,2 +1,2 @@
-scp 172.31.32.219:test_insert.sql
-ssh 172.31.32.219 /root/test_insert.sql
+scp /var/lib/jenkins/workspace/Jenkins-Test/test/test_insert.sql 172.31.32.219:/tmp/test_insert.sql
+ssh 172.31.32.219 /tmp/test_insert.sql
